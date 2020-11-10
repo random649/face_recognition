86d2ef4d9468d9c10c04784bbcb3fe87108b9611
zhouzp16 <zhouzp16@163.com>
2020-11-10 11:59:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ import numpy as np
 import torch
 from model import Backbone, ArcFace, FocalLoss
 import torchvision.transforms as transforms
+import tqdm
+
+import config
 
 
 def get_feature(img_path, transform, model, device):
@@ -37,7 +40,7 @@ def cal_accuracy(sims, labels):
 
 def test(args):
     device = torch.device(('cuda:%d'%args.gpu) if torch.cuda.is_available() else 'cpu')
-    BACKBONE = Backbone([args.input_size, args.input_size])
+    BACKBONE = Backbone([args.input_size, args.input_size], args.num_layers, args.mode)
     BACKBONE.load_state_dict(torch.load(args.ckpt_path))
     BACKBONE.to(device)
     BACKBONE.eval()
@@ -49,8 +52,8 @@ def test(args):
         pairs = f.readlines()
     sims = []
     labels = []
-    for pair_id, pair in enumerate(pairs):
-        print('processing %d/%d...' % (pair_id, len(pairs)), end='\r')
+    for pair_id, pair in tqdm.tqdm(enumerate(pairs)):
+        # print('processing %d/%d...' % (pair_id, len(pairs)), end='\r')
         splits = pair.split()
         feat1 = get_feature(os.path.join(args.data_root, splits[0]), transform, BACKBONE, device)
         feat2 = get_feature(os.path.join(args.data_root, splits[1]), transform, BACKBONE, device)
@@ -64,13 +67,18 @@ def test(args):
 
 
 if __name__ == '__main__':
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '8'
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_root', type=str, default='data/lfw')
-    parser.add_argument('--pair_file', type=str, default='data/lfw_test_pair.txt')
+    parser.add_argument('--data_root', type=str, default=config.TEST_DATA)
+    parser.add_argument('--pair_file', type=str, default=config.TEST_PAIR)
     parser.add_argument('--gpu', type=int, default=0)
     parser.add_argument('--input_size', type=int, default=112)
     parser.add_argument('--emb_dims', type=int, default=512)
-    parser.add_argument('--ckpt_path', type=str, default='ckpt/backbone_epoch0.pth')
+    parser.add_argument('--num_layers', type=int, default=152, choices=(50, 100, 152))
+    parser.add_argument('--mode', type=str, default='ir', choices=('ir', 'ir_se'))
+    parser.add_argument('--ckpt_path', type=str, default=os.path.join(config.TEST_CKPT, 'backbone_epoch{}.pth'.format(config.TEST_EPOCH)))
     args = parser.parse_args()
     print(str(args))
     test(args)
